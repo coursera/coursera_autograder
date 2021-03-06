b33996f7df2f96ebed7fc584aa2c082347522c45
Joseph Li <jli@coursera.org>
2021-03-05 16:24:33 -0800

    updating pip package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='coursera_autograder',
-      version='0.20.0',
+      version='0.1.0',
       description='A toolkit to help develop asynchronous graders for Coursera\
           based on docker images.',
       long_description=readme(),
@@ -18,8 +18,8 @@ setup(name='coursera_autograder',
       ],
       keywords='grading programming coursera sdk docker cli tool',
       url='https://github.com/coursera/coursera_autograder',
-      author='Nikhil Garg',
-      author_email='ngarg@coursera.org',
+      author='Joseph Li',
+      author_email='jli@coursera.org',
       license='Apache',
       entry_points={
           'console_scripts': [
@@ -28,11 +28,11 @@ setup(name='coursera_autograder',
       },
       packages=['coursera_autograder', 'coursera_autograder.commands'],
       install_requires=[
-          'dockerfile-parse==0.0.6',
-          'docker-py==1.10.4',
-          'requests==2.9.2',
-          'requests-toolbelt==0.7.1',
-          'semver==2.7.5',
+          'dockerfile-parse>=0.0.6',
+          'docker-py>=1.10.4',
+          'requests>=2.9.2',
+          'requests-toolbelt>=0.7.1',
+          'semver>=2.7.5',
       ],
       test_suite='nose.collector',
       tests_require=['nose', 'nose-cover3'],
